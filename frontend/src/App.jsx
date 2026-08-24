3a16870ebc4b1bd4e83fc05d82c4cf9aad6d0652
nookarinchai <arinnookynky@gmail.com>
2026-08-24 16:40:02 +0700

    progress on userinfo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import PersonalInfo from "./pages/User/01MyAccount/PersonalInfo"
 
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
 
   return (
     <>
-     
+     <PersonalInfo/>
     </>
   )
 }
