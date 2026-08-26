8c9ba14e46cdfb6147f5d7bbbd5572e0dc17dd0f
nookarinchai <arinnookynky@gmail.com>
2026-08-26 10:42:31 +0700

    init product mockdata

---
+++ frontend/src/lib/product.js
@@ -0,0 +1,12 @@
+const PRODUCT = [
+      {
+    id: "1",
+    name: "GearVerse Pro Headset",
+    tag: "RGB • 7.1 Surround • Pink",
+    unitPrice: 89.99,
+    quantity: 1,
+    delivery: "Est. Delivery: 2-3 Business Days",
+    image: "/images/headset.jpg",
+  },
+
+]
