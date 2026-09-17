ae1653ad40018de568d0a505ec04e6881c35096c
B6001186 <atcharapornok@gmail.com>
2026-09-17 22:38:09 +0700

    modified index.js and index.js in v1 folder

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,9 +12,6 @@ const corsOptions = {
   credentials: true,
 };
 
- // URL ของ Frontend ที่ต้องการอนุญาต // allowing vercel domains
-// origin: "http://localhost:5173", // URL ของ Frontend ที่ต้องการอนุญาต (old code before deploy)
-
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -24,8 +21,6 @@ app.use(cors(corsOptions));
 
 app.use("/api", apiRoutes);
 
-
-
 //Centralize error handling middleware
 app.use((err, req, res, next) => {
   return res.status(500).json({
@@ -34,25 +29,19 @@ app.use((err, req, res, next) => {
   });
 });
 
-//==============old code==============
 async function start() {
   try {
+
     await connectDB();
 
     app.listen(port, () => {
       console.log(`Server running on port: ${port} 🏃‍♀️`);
     });
+
   } catch (err) {
     console.error("Failed to connect to MongoDB:", err.message);
     process.exit(1);
   }
 }
-//==============old code==============
-start();
-
-
-//==========new code=======================
-// connectDB().catch((err) => console.error("Failed to connect to MongoDB:", err.message));
-// export default app;
 
 start();
--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -1,32 +1,24 @@
 import { Router } from "express";
 import { userRouter } from "./users.routes.js";
 import { productRouter } from "./products.routes.js";
-import { promoRouter } from "./promo.routes.js";
 import { reviewRouter } from "./reviews.routes.js";
 import { shoppingCartRouter } from "./shoppingcart.routes.js";
 import { categoryRouter } from "./categories.routes.js";
 import { orderRouter } from "./orders.routes.js";
 import { colorRouter } from "./colors.routes.js";
 import { kbswitchRouter } from "./kb-switch.routes.js";
-// นำเข้า Router ของ Keycap และ Tag ที่สร้างขึ้นใหม่
 import { keycapRouter } from "./keycap.routes.js";
-import { tagRouter } from "./tags.routes.js";
 import { wishlistRouter } from "./wishlists.routes.js";
 
 export const router = Router();
 
-// กำหนดเส้นทาง API ของแต่ละโมดูล
 router.use('/users', userRouter);
 router.use('/products', productRouter);
-router.use('/promo', promoRouter);
 router.use('/reviews', reviewRouter);
 router.use('/shoppingcart', shoppingCartRouter);
 router.use('/categories', categoryRouter);
 router.use('/orders', orderRouter);
 router.use('/colors', colorRouter);
 router.use('/kbswitch', kbswitchRouter);
-// เชื่อมต่อเส้นทาง API สำหรับจัดการ Keycap (/api/v1/keycaps)
 router.use('/keycaps', keycapRouter);
-// เชื่อมต่อเส้นทาง API สำหรับจัดการ Tag สินค้า (/api/v1/tags)
-router.use('/tags', tagRouter);
 router.use('/wishlists', wishlistRouter)
